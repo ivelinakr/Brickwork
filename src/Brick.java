4a95677dc394c4cc666d3c367ecfe8ec9b27f0c3
Ivelina <ivelina>
2020-12-24 14:15:57 +0200

    Created Brick class with getBricks() that finds all bricks in layer (uses coordinates)

---
+++ src/Brick.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class Brick {
+    public int[] coordinate1 = new int[2];
+    public int[] coordinate2 = new int[2];
+
+    public Brick(int[] coordinate1, int[] coordinate2) {
+        this.coordinate1 = coordinate1;
+        this.coordinate2 = coordinate2;
+    }
+
+    public int[] getC1() {
+        return coordinate1;
+    }
+
+    public int[] getC2() {
+        return coordinate2;
+    }
+
+    public static Brick[] getBricks(int[][] layout, int length, int width) {
+        int brickCount = (length*width)/2;
+        Brick[] bricks = new Brick[brickCount];
+        ArrayList<Brick> tempArrList = new ArrayList<>();
+        for (int i=0; i<length; i++) {
+            for (int j=0; j<width; j++) {
+                int current = layout[i][j];
+                if (j != width-1) {
+                    int down = layout[i][j+1];
+                    if (current == down) {
+                        int[] c1 = {i,j};
+                        int[] c2 = {i,j+1};
+                        Brick rightB = new Brick(c1,c2);
+                        tempArrList.add(rightB);
+                        tempArrList.add(new Brick(c1,c2));
+                    }
+                }
+                if (i != length-1) {
+                    int right = layout[i+1][j];
+                    if (current == right) {
+                        int[] c1 = {i,j};
+                        int[] c2 = {i+1,j};
+                        Brick rightB = new Brick(c1,c2);
+                        tempArrList.add(rightB);
+                    }
+                }
+            }
+        }
+        bricks = tempArrList.toArray(bricks);
+
+        return bricks;
+    }
+}
