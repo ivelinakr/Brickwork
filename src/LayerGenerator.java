8cbb5b06d78c60f3dfcb19df68b782df76c16ba4
Ivelina <ivelina>
2020-12-26 23:59:04 +0200

    Created getPrintLayout() - takes layout and prints it to console

--- src/LayerGenerator.java
+++ src/LayerGenerator.java
@@ -34,12 +34,12 @@ public class LayerGenerator {
                         for (Brick brick : layer1) {
                             if (Arrays.equals(brick.getC1(),lastFirstRow) && Arrays.equals(brick.getC2(),lastSecondRow)) {
                                 // |=
-                                int[] firstC1 = {5,0};
-                                int[] firstC2 = {5,1};
-                                int[] secondC1 = {6,0};
-                                int[] secondC2 = {7,0};
-                                int[] thirdC1 = {6,1};
-                                int[] thirdC2 = {7,1};
+                                int[] firstC1 = {length-2,j};
+                                int[] firstC2 = {length-2,j+1};
+                                int[] secondC1 = {length-1,j};
+                                int[] secondC2 = {length,j};
+                                int[] thirdC1 = {length-1,j+1};
+                                int[] thirdC2 = {length,j+1};
 
                                 Brick firstBrick = new Brick(firstC1, firstC2);
                                 Brick secondBrick = new Brick(secondC1, secondC2);
@@ -54,8 +54,7 @@ public class LayerGenerator {
                             }
                         }
                     }
-                    String C2 = Arrays.toString(layer1[brL1Num].getC2());
-                    String right = Arrays.toString(rightB);
+
                     if ((Arrays.equals(layer1[brL1Num].getC1(), c1l2) && Arrays.equals(layer1[brL1Num].getC2(), rightB)) || i == length - 1) {
                         Brick newBrick = new Brick(c1l2, lowerB);
                         tempArrList.add(newBrick);
@@ -80,9 +79,100 @@ public class LayerGenerator {
 
         System.out.println("LAYER 2:");
         for (int m=0; m<brickCount; m++) {
+            bricks2[m].setNumber(m+1);
             System.out.println("BRICK NUMBER "+(m+1)+"="+Arrays.toString(bricks2[m].getC1())+" "+Arrays.toString(bricks2[m].getC2()));
         }
 
         return bricks2;
+    }
+
+    public static void getPrintLayout(Brick[] layer, int length, int width) {
+        ArrayList<String> rows = new ArrayList<>();
+
+        for (int m=0; m<layer.length; m++) {
+            layer[m].setNumber(m+1);
+        }
+
+        for (int i=0;i<width;i++) {
+            String row = "";
+            ArrayList<Brick> tempArrList = new ArrayList<>();
+
+            // check if its on the current row
+            for (int k=0;k<layer.length;k++) {
+                int[] C1YCoordinate = layer[k].getC1();
+                int[] C2YCoordinate = layer[k].getC2();
+                // if it is, add to list
+                if (C1YCoordinate[1]==i || C2YCoordinate[1]==i) {
+                    tempArrList.add(layer[k]);
+                }
+            }
+
+            // for getting formatted row
+            for (int l=0;l<tempArrList.size();l++) {
+                int[] C1CurrentBlock = tempArrList.get(l).getC1();
+                int[] C2CurrentBlock = tempArrList.get(l).getC2();
+                int brickNumber = tempArrList.get(l).getNumber();
+
+                if (C1CurrentBlock[1]==C2CurrentBlock[1]) {
+                    if (brickNumber<10) {
+                        row = row + "* " + tempArrList.get(l).getNumber() + "   " + tempArrList.get(l).getNumber() + "   ";
+                    } else if (brickNumber>=10 && brickNumber<=99) {
+                        row = row + "* " + tempArrList.get(l).getNumber() + "  " + tempArrList.get(l).getNumber() + "   ";
+                    }
+                } else {
+                    if (brickNumber<10) {
+                        row = row + "* " + tempArrList.get(l).getNumber() + "   ";
+                    } else if (brickNumber>=10 && brickNumber<=99) {
+                        row = row + "* " + tempArrList.get(l).getNumber() + "  ";
+                    }
+                }
+
+                if (l==tempArrList.size()-1) {
+                    row = row + "*";
+                }
+            }
+            rows.add(row);
+        }
+
+        // for getting horizontal line
+        String horizontalLine = "*";
+        for (int i=0; i<layer.length; i++) {
+            // skips last
+            if (i == layer.length-1) {
+                break;
+            }
+            // if brick is horizontal on first layer add 10 asterisks, if vertical 6
+            if (layer[i].getC1()[0]==0 && layer[i].getC2()[0]==0) {
+                horizontalLine = horizontalLine + "**********";
+            } else {
+                horizontalLine = horizontalLine + "******";
+            }
+        }
+        System.out.println(horizontalLine);
+
+        // for getting asterisks in between rows
+        for (int i=0; i<rows.size(); i++) {
+            System.out.println(rows.get(i));
+            String middle = "";
+
+            for (int j=0; j<rows.get(i).length(); j++) {
+                if (i==rows.size()-1) {
+                    break;
+                } else if (rows.get(i).charAt(j)=='*') {
+                    middle = middle + "*";
+                } else if (rows.get(i).charAt(j)==' ') {
+                    continue;
+                } else if (Character.isDigit(rows.get(i).charAt(j)) && rows.get(i).charAt(j)==rows.get(i+1).charAt(j)) {
+                    middle = middle + "     ";
+                } else {
+                    middle = middle + "*********";
+                    j=j+7;
+                }
+            }
+            if (middle!="") {
+                System.out.println(middle);
+            }
+        }
+        System.out.println(horizontalLine);
     }
 }
